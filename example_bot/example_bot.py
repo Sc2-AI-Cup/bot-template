db199d1cfe3ca49744278330ced17d51b7a4b1a0
Tomáš Mikula <mail@tomasmikula.cz>
2020-10-17 20:24:51 +0200

    Init commit

---
+++ example_bot/example_bot.py
@@ -0,0 +1,12 @@
+import sc2
+from sc2.bot_ai import BotAI
+
+
+class ExampleBot(BotAI):
+    '''
+    Template bota
+    '''
+
+    async def on_step(self, iteration):
+        '''Metoda provedena každou iterací hry'''
+        pass
